b30a07e57809c1bbc6cc940ba5e976b361b0730b
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-09-28 00:44:24 +0000

    aaaaaaaaaaa eg get ekki meir i bili.. need a break

--- brain storm.py
+++ brain storm.py
@@ -14,19 +14,21 @@ cGO=0 												#-II- fyrir myndavel. tharf ad baeta fleiri breytum sem stjorn
 mwGO=0  											#-II- fyrir hvort modWatch se virkt.
 msGO=0 												#-II- fyrir hvort modstuff se virkt.
 velocity=63 										#50% af max.
+voice=0 											#voice=channel. það channel sem er núna í notkun.
 status=8*[8*[0]]									#heldur utan um hvada notur eru merktar sem thekktar.
 mod=8*[8*[0]] 										#mun halda utan um upplysingar hverrar notu sidar.
 skali=[60, 62, 64, 65, 67, 69, 71, 72] 				#skali, nuna c dur. seinna a ad geta valid.
 liveplay=0 											#hvort thetta se i liveplay mode eda sequencer mode.
 a=0 												
 b=0 												#global breyturnar a og b eru hnit fyrir notu i modWatch.
-#Startup only end
+#Startup only end  		--- þarf sennilega að breyta status í 8x8x16 fylki til að halda utan um voices.
+#						--- Þarf sennilega amk 8x8x8 fyrir mod lika til að halda utan um nægar upplysingar. x,y hnit og z mod factor. fylki með gildum ekki 1 og 0.
 
 	
    
 #trellisWatch begins 	--- fylgist med tokkum a trellis.
 def trellisWatch():
-	global tGO, status								#global breytur, útskýrðar efst.
+	global tGO, status, voice						#global breytur, útskýrðar efst.
 	if tGO==1:																						#-----------ATHUGA
 		time.sleep(0.03) 							#bid sem var alltaf i synidaemum og gaeti kannski thurft ad auka.
 		if trellis.readSwitches():					#les hvort thad hafir verid ytt a EINHVERN takka 
@@ -58,7 +60,7 @@ def myndavel ():
 
 
 
-#playcolumn begin --- spilar notur i dalk og takt maelinn lika.
+#playcolumn begins --- spilar notur i dalk og takt maelinn lika.
 def playColumn(dalkur):
 	global tempo, FLASH, status, tGO, msGO			#global breytur, útskýrðar efst.
 	tGO=0 											#slekkur a trellisWatch.
@@ -82,11 +84,12 @@ def playColumn(dalkur):
 													#velocity er valið 0 vegna þess að sum midi hljóðfæri nota ekki message-ið note off heldur bara velocity 0.
 	tGO=1 											#kveikir á trellisWatch.
 	time.sleep(tempo*lengd) 						#tíminn milli lok nótu og upphaf næstu.
-#translate status to tones end
+#playColumn ends 		--- þarf að breyta status i 8x8x16 og deala við það. 
+# 						--- finna út hvernig á að deala við mismunandi takta nótna.
 
 
 
-#taktmaelir begin
+#taktmaelir begins
 def taktmaelir(dalkur) :
 	global FLASH, status							#global breytur, útskýrðar efst.
 	for x in range (0,8):							#fyrir öll LED í 'dálkur'
@@ -97,48 +100,73 @@ def taktmaelir(dalkur) :
 		trellis.clrLED(x) 							#slökkva á LED!
 	trellis.writeDisplay()							#uppfæra led á borði.. VERÐI MYRKUR!
 #taktmaelir end  		--- vantar ad kveikja a ljosum aftur sem voru tharna fyrir. eda excluda thau.
+#						--- öllu helst að bua til fylki LED sem heldur utanum hvaða ljos eiga að vera i gangi currently.
 
 
 
 #SEQUENCER LOOP, THIS IS IT YO GUYS:
 def Sequencer():									
-	if (livemode == 0 and modmode == 0): 			#skoðar hvort AVON vinnslurými eigi að vera í sequencer-, live- eða modemode.
-		for dalkur in range (0,7): 					#fyrir alla dálka í sequencer.
-			playColumn(dalkur) 						#spila nótur dálks auk bið og taktmælis.
-			if (if livemode == 1 or modmode == 1): 	#skoðar hvort það hafi verið breyting á hvaða mode er i gangi.
-				break 								#ef svo er, stöðvum við loopuna.
+	if (livemode == 0 and modmode == 0  			#skoðar hvort AVON vinnslurými eigi að vera í sequencer-, live- eða modemode.
+	    	and stop == 0):	
+		for dalkur in range (0,7): 						#fyrir alla dálka í sequencer.
+			playColumn(dalkur) 							#spila nótur dálks auk bið og taktmælis.
+			if (livemode == 1 or modmode == 1 			#skoðar hvort það hafi verið breyting á hvaða mode er i gangi.
+			or stop == 1) 								#a ad stodva allt?
+				break 									#ef svo er, stöðvum við loopuna.
+			sequencer() 								#förum aftur i sequencer. byrjun fallsins finnur ut hvort þurfi að flytja vinnslusvæðið.
 	elif livemode == 1: 							#ef live mode er 1
 		livePlay() 									#förum við í liveplay, sem er til að spila on the fly.
 	elif modmode == 1: 								#ef modmode er 1
 		modWatch() 									#förum við í modwatch.
-	Sequencer() 									#annars/eftir að spila i gegnum alla dálka, förum við aftur i sequencer. "hala"endurkvæmt fall.
+	elif stop == 1:									#effectively drepur sequencerinn.
+		pass 										#þvi pass gerir ekkert og þráðurinn endar eftir þetta.
+	else:	
+		Sequencer() 									#annars/eftir að spila i gegnum alla dálka, förum við aftur i sequencer. "hala"endurkvæmt fall.
 #SEQUENCER END, BOOOOOOOOOIIII
+
+
+
+#clearVoice begins 		--- það voice sem er í gangi er eytt út.
+def clearVoice(voice):
+#clearVoice ends
+
+
+
+#clearAll begins 		--- eyðir öllum voice-um.
+def clearAll():
+#clearAll ends
 
 
 
 #modWatch begins 		--- her kemur mod vinnsla fyrir hverja notu fyrir sig. veit ekki hvar vid munum vinna med voice.
 def modWatch():
-	global a, b 									#global breytur, útskýrðar efst
-
-#modStuff ends
+	global a, b 									#global breytur, útskýrðar efst	
+#modStuff ends    		--- vantar allt! .... nema a og b :)
 
 
 
 #livePlay begins 		--- her er megin vinnslan fyrir liveplay partinn.
 def livePlay():
-	blah
-#livePlay ends
+#livePlay ends  		--- vantar allt
 
 
 
+#menuWatch begins 		--- vantar betra nafn á þetta fall. fylgist með öllum utanaðkomandi inputum nema trellis og myndavél. þ.e. knob og tökkunum
+def menuWatch():
+#menuWatch ends
+
+
+	
 #multithread starts		--- partur af main.
 t1=thread(target=trellisWatch)
 t2=thread(target=myndavel)
 t3=thread(target=modWatch)
+t4=thread(target=menuWatch)
 t1.start()
 t2.start()
 t3.start()
-#multithread ends
+t4.start()
+#multithread ends    --- breyta i function með if skilyrðum hvort þráður sé dauður eða ekki.
 
 
 
