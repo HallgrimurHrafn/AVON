5ad9f412942fe3768e0f34d7eca03fcc6f70fbff
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-07 01:32:54 +0000

    Updated through script

--- CLICK-TO-UPDATE-LOCAL-bragi.sh
+++ CLICK-TO-UPDATE-LOCAL-bragi.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 cd ~/documents/AVON
-read -rsp $'Press enter to continue...\n'
 git fetch origin
-read -rsp $'Press enter to continue...\n'
 git checkout master
-read -rsp $'Press enter to continue...\n'
 git merge origin/master
-read -rsp $'Press enter to continue...\n'
