dc133b597a49b01ab8faf7975e7e841d436202c5
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-18 20:15:06 +0000

    Updated through script

---
+++ int-test.py
@@ -0,0 +1,22 @@
+from time import sleep
+import RPi.GPIO as GPIO
+
+pin =7  #ma stilla a flest allt held eg. endilega prufa.
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+
+def my_callback(channel):
+    print("UPDate!")
+
+                                    # stop detection for 0.1 sec
+    GPIO.remove_event_detect(pin)     # þessum 2 linum ma mögulega sleppa. ef forritid virkar. prufa ad komenta ut linur
+    sleep(0.1)                        # 14-16 og sja hvort thad virki enn.
+    GPIO.add_event_detect(pin, GPIO.RISING, callback=my_callback, bouncetime=300)
+
+GPIO.add_event_detect(pin, GPIO.RISING, callback=my_callback, bouncetime=300) #bouncetime.. lesa https://sourceforge.net/p/raspberry-gpio-python/wiki/Inputs/
+
+# you can continue doing other stuff here
+while True:
+    pass
