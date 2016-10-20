36e5fc19c6477e4d422cc2fc82cf4570df07e77e
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-20 22:07:23 +0000

    Updated through script

--- demo.py
+++ demo.py
@@ -73,17 +73,18 @@ def playColumn(dalkur):
 def taktmaelir(dalkur) :
 	global FLASH, status, voice						#global breytur, utskyrdar efst.
 	for x in range (0,8):							#fyrir oll LED i 'dalkur'
-		#if status[dalkur][x][voice]==0:				#gert svo vid seum bara ad kveikja a led-um sem var slokkt a fyrir.
-			
-			
-		trellis.setLED(tfOut(x*8+dalkur))				#kveikja a LED!
+		if status[dalkur][x][voice]==1:				#gert svo vid seum bara ad kveikja a led-um sem var slokkt a fyrir.
+			trellis.clrLED(tfOut(x*8+dalkur))
+		else:	
+			trellis.setLED(tfOut(x*8+dalkur))				#kveikja a LED!
 		#print(x*8+dalkur,tfOut(x*8+dalkur), 'on')
 	trellis.writeDisplay() 							#uppfaera led a bordi.. VERDI LJOS!
 	time.sleep(FLASH) 								#bidtimi eftir taktmaelis flash.
 	for x in range (0,8): 							#fyrir oll LED i 'dalkur'
-		if status[dalkur][x][voice]==0: 			#gert svo vid seum bara ad slokkva a led-um sem ekki var kveikt a fyrir "taktmaelir".
-			x=x
-			trellis.clrLED(tfOut(x*8+dalkur)) 				#slokkva a LED!
+		if status[dalkur][x][voice]==1: 			#gert svo vid seum bara ad slokkva a led-um sem ekki var kveikt a fyrir "taktmaelir".
+			trellis.setLED(tfOut(x*8+dalkur))
+		else:
+			trellis.clrLED(tfOut(x*8+dalkur)) 		#slokkva a LED!
 		#print(x*8+dalkur,tfOut(x*8+dalkur), 'off')
 	trellis.writeDisplay()							#uppfaera led a bordi.. VERDI MYRKUR!
 #taktmaelir end
