7ab48a05b5b1e9320afe04b3b8b698c30a53f939
thatmooglie <bragimarinosson@gmail.com>
2016-10-20 21:06:10 +0000

    Updated through script

---
+++ screen/screentest.py
@@ -0,0 +1,31 @@
+from PIL import Image
+
+import Adafruit_ILI9341 as TFT
+import Adafruit_GPIO as GPIO
+import Adafruit_GPIO.SPI as SPI
+
+# Raspberry Pi config.
+DC = 18
+RST = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# TFT LCD display class
+disp = TFT.Adafruit_ILI9341(DC, rst=RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=64000000))
+
+# Initialize display.
+disp.begin()
+
+#clear to white background
+disp.clear((255,255,255))
+
+# Load and image
+print('Loading image')
+image = Image.open('avon.png')
+
+# Resize and rotate
+image = image.rotate(90).resize((240,320))
+
+# Draw image
+print('Drawing image')
+disp.display(image)
