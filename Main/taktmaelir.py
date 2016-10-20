a46bcfca91f2775b553a2aabaad85b8cd5bb82e3
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-20 15:14:49 +0000

    Updated through script

--- Main/taktmaelir.py
+++ Main/taktmaelir.py
@@ -1,13 +1,62 @@
 #taktmaelir begins
+import time
+FLASH=1
+voice=0
+
+
 def taktmaelir(dalkur) :
-	global FLASH, status, voice						#global breytur, útskýrðar efst.
-	for x in range (0,8):							#fyrir öll LED í 'dálkur'
-		if status[dalkur][x][voice]==0:				#gert svo við séum bara að kveikja a led-um sem var slökkt á fyrir.
-			trellis.setLED(x*8+dalkur)				#kveikja á LED!
-	trellis.writeDisplay() 							#uppfæra led á borði.. VERÐI LJÓS!
-	time.sleep(FLASH) 								#biðtími eftir taktmælis flash.
-	for x in range (0,8): 							#fyrir öll LED í 'dálkur'
-		if status[dalkur][x][voice]==0: 			#gert svo við séum bara að slökkva á led-um sem ekki var kveikt á fyrir "taktmaelir".
-			trellis.clrLED(x*8+dalkur) 				#slökkva á LED!
-	trellis.writeDisplay()							#uppfæra led á borði.. VERÐI MYRKUR!
+	global FLASH, status, voice						#global breytur, utskyrdar efst.
+	for x in range (0,8):							#fyrir oll LED i 'dalkur'
+		if status[dalkur][x][voice]==0:				#gert svo vid seum bara ad kveikja a led-um sem var slokkt a fyrir.
+			trellis.setLED(x*8+dalkur)				#kveikja a LED!
+		#print(x*8+dalkur,tfOut(x*8+dalkur), 'on')
+	trellis.writeDisplay() 							#uppfaera led a bordi.. VERDI LJOS!
+	time.sleep(FLASH) 								#bidtimi eftir taktmaelis flash.
+	for x in range (0,8): 							#fyrir oll LED i 'dalkur'
+		if status[dalkur][x][voice]==0: 			#gert svo vid seum bara ad slokkva a led-um sem ekki var kveikt a fyrir "taktmaelir".
+			trellis.clrLED(x*8+dalkur) 				#slokkva a LED!
+		#print(x*8+dalkur,tfOut(x*8+dalkur), 'off')
+	trellis.writeDisplay()							#uppfaera led a bordi.. VERDI MYRKUR!
 #taktmaelir end
+
+
+#tfIn begins 		--- varpar ur trellis i okkar format.
+def tfIn (a):
+	f=a//16
+	d=(a%16)%4
+	l=(a%16)//4
+	if f%2==0:
+		b=16*f+8*l+d
+	else:
+	#	#b=16*(f+1)-1-(3-l)*8-3+d
+		b=16*(f+1)-(3-l)*8+d-4
+
+	#b=16*(f+f%2)+8*l+d -28*(f%2)
+	return b
+#tfIn ends	
+
+
+
+#tfOut begins 		--- varpar ur okkar formati i trellis.
+def tfOut (a):
+	f=a//16
+	d=(a%16)%8
+	l=(a%16)//8
+	if d<4:
+		if f<2:
+			b=8*f+4*l+d
+		else:
+			if f==2:
+				b=32+4*l+d
+			else:
+				b=40+d+4*l
+	else:
+		if f%2==1:
+			b=16*(f+1)+d-4*(3-l)
+		else:
+			b=16*(f+1)+d-4*(3-l)+8
+	return b
+#tfOut ends
+
+for x in range (0,8):
+	taktmaelir(x)
