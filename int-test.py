bcc5ce926dc6ae4b258e32c4285ad80af56b13d8
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-21 00:21:01 +0000

    Updated through script

--- int-test.py
+++ int-test.py
@@ -6,9 +6,14 @@ pin =37  #ma stilla a flest allt held eg. endilega prufa. thetta er int virinn u
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-while True:
-    time.sleep(0.01)
-	if (GPIO.input(37) == GPIO.HIGH):
-		print('high')
+def blah():
+	time.sleep(0.01)
+	if GPIO.input(37) == GPIO.HIGH:
+		print('high')	
 	if GPIO.input(37) == GPIO.LOW:
 		print('low')
+
+
+
+while True:
+	blah()
