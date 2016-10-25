256b96a295ca7fb7b27b1ca2217107cb3402f635
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-25 15:57:35 +0000

    Updated through script

--- test.py
+++ test.py
@@ -14,8 +14,8 @@ ser = serial.Serial(
 counter =0
 while 1:
     time.sleep(1)
-    ser.write(struct.pack(hex("\x80\x3C\x64")))
+    ser.write(struct.pack("\x80\x3C\x64"))
     time.sleep(1)
-    ser.write(struct.pack(hex("\x90\x3C\x00")))
+    ser.write(struct.pack("\x90\x3C\x00"))
     counter += 1
     #print(counter)
