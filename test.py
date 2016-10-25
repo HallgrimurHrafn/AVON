c2999f47e6686ae769786df57e276b04c5f19591
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-25 15:25:58 +0000

    Updated through script

--- test.py
+++ test.py
@@ -4,14 +4,15 @@ import serial
 import pygame
 
 ser = serial.Serial(
-	port='/dev/ttyAMA0',
-    baudrate = 31250,
+	port='/dev/serial1', #eda sys0 eda hvad sem hinn var.
+    baudrate = 38400,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
     timeout=1)
 counter =0
 while 1:
-	ser.write('Write counter: %d \n'%(counter))
+	#ser.write('Write counter: %d \n'%(counter))
 	time.sleep(1)
 	counter += 1
+	print(counter)
