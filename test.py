54cc09aa5b1b50ecb41fd7442514d4a817be2fd7
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-26 17:25:11 +0000

    Updated through script

--- test.py
+++ test.py
@@ -17,9 +17,11 @@ ser = serial.Serial(
 while 1:
     time.sleep(1)
     x=ser.write(mido.Message('note_on', channel=0, note=60, velocity=100).hex())
+    ser.flush()
     print(mido.Message('note_on', channel=0, note=60, velocity=100).hex())
     print(x)
     time.sleep(1)
     x=ser.write(mido.Message('note_off', channel=0, note=60, velocity=0).hex())
+    ser.flush()
     print(mido.Message('note_off', channel=0, note=60, velocity=0).hex())
     print(x)
