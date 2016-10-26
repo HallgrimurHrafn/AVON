0381bfc12d850227f86b16e30dcb0e08f00a79f2
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-26 17:23:00 +0000

    Updated through script

--- test.py
+++ test.py
@@ -7,11 +7,11 @@ import struct
 
 ser = serial.Serial(
     port='/dev/serial1', #eda sys0 eda hvad sem hinn var.
-    baudrate = 38400,
+    baudrate = 31250,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
-    #timeout=1
+    timeout=1
     )
 #counter =0
 while 1:
