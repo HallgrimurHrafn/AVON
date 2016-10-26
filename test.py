57ce767b64b969501e80e1fd0e8f1b86c5e3c635
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-10-26 17:43:31 +0000

    Updated through script

--- test.py
+++ test.py
@@ -7,20 +7,24 @@ import struct
 
 ser = serial.Serial(
     port='/dev/serial1', #eda sys0 eda hvad sem hinn var.
-    baudrate = 31250,
+    baudrate = 38400,
     parity=serial.PARITY_NONE,
     stopbits=serial.STOPBITS_ONE,
     bytesize=serial.EIGHTBITS,
     timeout=1
     )
 #counter =0
+
+
 while 1:
     time.sleep(1)
+    ser.flushInput()
     x=ser.write(mido.Message('note_on', channel=0, note=60, velocity=100).hex())
     ser.flush()
     print(mido.Message('note_on', channel=0, note=60, velocity=100).hex())
     print(x)
     time.sleep(1)
+    ser.flushInput()
     x=ser.write(mido.Message('note_off', channel=0, note=60, velocity=0).hex())
     ser.flush()
     print(mido.Message('note_off', channel=0, note=60, velocity=0).hex())
