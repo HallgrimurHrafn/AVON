d1ae9fcb9f2d3ab8fcddb04a1769913b717cbf72
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-01 21:29:51 +0000

    Updated through script

--- demo.py
+++ demo.py
@@ -321,26 +321,22 @@ def liveSet():
         y=tfIn(x)
         status[voice][y % 8][y // 8]=0
     ledshow(np.zeros((8, 8)))
-    GPIO.add_event_detect(37, GPIO.FALLING, callback=liveplay)  # kannski tharf thetta ad vera gpio.both
+    GPIO.add_event_detect(37, GPIO.BOTH, callback=liveplay)  # kannski tharf thetta ad vera gpio.both
 # done
 def liveplay(channel):
     global skali, voice, nowPlaying
-    time.sleep(0.005)
+    time.sleep(0.015)
     if trellis.readSwitches():
         for x in range(0, 64):
             y=tfIn(x)
             if trellis.justPressed(x):
-                # if nowPlaying[y%8][y//8] == 0:
-                    # nowPlaying[y%8][y//8]=1
-                    #print ('on', 'channel er', voice, 'notan er', skali[x], 'velocity er', 100)
-                    trellis.setLED(x)
+                #print ('on', 'channel er', voice, 'notan er', skali[x], 'velocity er', 100)
+                trellis.setLED(x)
             if trellis.justReleased(x):
-                # if nowPlaying[y%8][y//8] == 1:
-                    # nowPlaying[y%8][y//8] = 0
-                    #print ('off', 'channel er', voice, 'notan er', skali[x], 'velocity er', 0)
-                    trellis.clrLED(x)
+                #print ('off', 'channel er', voice, 'notan er', skali[x], 'velocity er', 0)
+                trellis.clrLED(x)
         trellis.writeDisplay()
-    time.sleep(0.005)
+    time.sleep(0.015)
     trellis.readSwitches()
 #
 
