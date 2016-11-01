7ab9bf2d89a1338557c01237e34bcc955c28dab2
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-01 23:40:37 +0000

    Updated through script

--- rotary.py
+++ rotary.py
@@ -8,6 +8,6 @@ GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary click
 def test(channel):
     print('yeei')
 
-GPIO.add_event_detect(35, GPIO.FALLING, callback=test)
+GPIO.add_event_detect(35, GPIO.RISING, callback=test)
 while True:
     time.sleep(10)
