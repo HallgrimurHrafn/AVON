17fb3d94a7995c70caae37be76bdfd7b0c799cde
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-01 23:35:27 +0000

    Updated through script

--- rotary.py
+++ rotary.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -7,4 +8,6 @@ GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary click
 def test(channel):
     print('yeei')
 
-GPIO.add_event_detect(36, GPIO.FALLING, callback=test, bouncetime=100)
+GPIO.add_event_detect(35, GPIO.FALLING, callback=test, bouncetime=100)
+while True:
+    time.sleep(10)
