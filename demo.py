3fd5c8c7d0072c29fca362c1b3159c2fe575bc0f
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-01 20:46:21 +0000

    Updated through script

--- demo.py
+++ demo.py
@@ -246,8 +246,8 @@ def trellisWatch(channel):                              # ignore channel...
             time.sleep(0.015)                           #tilraun til ad laga response time-id. ma prufa ad fjarlaegja
             trellis.readSwitches()                      #tilraun til ad laga response time-id. ma prufa ad fjarlaegja
         else:
-            time.sleep(0.015)
-            trellis.readSwitches()
+            # time.sleep(0.015)
+            # trellis.readSwitches()
             trellisWatch(channel)
 # trellisWatch ends --------------------------------------
 
