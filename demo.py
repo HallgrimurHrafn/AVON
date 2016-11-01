d93518c716e654d66dec6cefa9e09268d4909bdb
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-01 13:24:21 +0000

    Updated through script

--- demo.py
+++ demo.py
@@ -392,7 +392,7 @@ GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up START button
 GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up TAP button
 
 fylki=np.zeros((8, 8))
-for x in range(0, 1):
+for x in range(0, 3):
     ledshow(fylki)
 
 for x in range(0, 64):
