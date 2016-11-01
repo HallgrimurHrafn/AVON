2b806539bdccdc6705442aecda0b57d653164357
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-01 21:25:10 +0000

    Updated through script

--- demo.py
+++ demo.py
@@ -329,18 +329,18 @@ def liveSet():
 # done
 def liveplay(channel):
     global skali, voice, nowPlaying
-    time.sleep(0.015)
+    # time.sleep(0.015)
     if trellis.readSwitches():
         for x in range(0, 64):
             y=tfIn(x)
             if trellis.justPressed(x):
-                if nowPlaying[y%8][y//8] == 0:
-                    nowPlaying[y%8][y//8]=1
+                # if nowPlaying[y%8][y//8] == 0:
+                    # nowPlaying[y%8][y//8]=1
                     #print ('on', 'channel er', voice, 'notan er', skali[x], 'velocity er', 100)
                     trellis.setLED(x)
             if trellis.justReleased(x):
-                if nowPlaying[y%8][y//8] == 1:
-                    nowPlaying[y%8][y//8] = 0
+                # if nowPlaying[y%8][y//8] == 1:
+                    # nowPlaying[y%8][y//8] = 0
                     #print ('off', 'channel er', voice, 'notan er', skali[x], 'velocity er', 0)
                     trellis.clrLED(x)
         trellis.writeDisplay()
