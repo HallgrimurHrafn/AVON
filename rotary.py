21b6dbe226f47a7b4bb2a5184525c12e0d2b7f4f
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-02 16:00:23 +0000

    Updated through script

--- rotary.py
+++ rotary.py
@@ -10,13 +10,18 @@ GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary right
 def test(channel):
     print('yeei')
 def left(channel):
+    GPIO.remove_event_detect(31)
     print('left')
+    GPIO.add_event_detect(31, GPIO.FALLING, callback=right, bouncetime=100)
+
 def right(channel):
+    GPIO.remove_event_detect(33)
     print('right')
+    GPIO.add_event_detect(33, GPIO.FALLING, callback=left, bouncetime=100)
 
 GPIO.add_event_detect(35, GPIO.RISING, callback=test, bouncetime=100)
 GPIO.add_event_detect(33, GPIO.FALLING, callback=left, bouncetime=100)
-GPIO.add_event_detect(31, GPIO.RISING, callback=right, bouncetime=100)
+GPIO.add_event_detect(31, GPIO.FALLING, callback=right, bouncetime=100)
 
 
 while True:
