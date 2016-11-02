a19aadd579aae041a6c29d8a03d22e01bcb9940f
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-02 17:03:00 +0000

    Updated through script

--- rotary.py
+++ rotary.py
@@ -6,14 +6,18 @@ GPIO.setmode(GPIO.BOARD)
 left = 33
 right = 31
 
+x=False
+
 GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary click
 GPIO.setup(left, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary left
 GPIO.setup(right, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary right
 
 def test(channel):
     print('yeei')
-def rotary(left_or_right):
-    print left_or_right
+def rotary(channel):
+    global x, left, right
+    x=(GPIO.input(left) and not GPIO.input(right)) and (GPIO.input(right) and not GPIO.input(left))
+    print x
 
 GPIO.add_event_detect(35, GPIO.RISING, callback=test, bouncetime=100)
 GPIO.add_event_detect(left, GPIO.FALLING, callback=rotary, bouncetime=20)
