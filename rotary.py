7583e2067bf3f8acb59e80467238254ba3c446f7
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-02 18:38:44 +0000

    Updated through script

--- rotary.py
+++ rotary.py
@@ -21,19 +21,25 @@ def test(channel):
     print('yeei')
 def rotary(channel):
     global x, left, right, cl, cr, lock
+    GPIO.remove_event_detect(left)
+    GPIO.remove_event_detect(right)
     if cl ==GPIO.input(left) and cr==GPIO.input(right):
+        GPIO.add_event_detect(left, GPIO.FALLING, callback=rotary, bouncetime=25)
+        GPIO.add_event_detect(right, GPIO.FALLING, callback=rotary, bouncetime=25)
         return
     cl=GPIO.input(left)
     cr=GPIO.input(right)
     if GPIO.input(right)==GPIO.input(left):
+        GPIO.add_event_detect(left, GPIO.FALLING, callback=rotary, bouncetime=25)
+        GPIO.add_event_detect(right, GPIO.FALLING, callback=rotary, bouncetime=25)
         return
     lock.acquire()
-    GPIO.remove_event_detect(channel)
     if GPIO.input(left)>GPIO.input(right):
         print 'left'
     elif GPIO.input(right)>GPIO.input(left):
         print 'right'
-    GPIO.add_event_detect(chanel, GPIO.FALLING, callback=rotary, bouncetime=25)
+    GPIO.add_event_detect(left, GPIO.FALLING, callback=rotary, bouncetime=25)
+    GPIO.add_event_detect(right, GPIO.FALLING, callback=rotary, bouncetime=25)
     lock.release()
 
 GPIO.add_event_detect(35, GPIO.RISING, callback=test, bouncetime=100)
