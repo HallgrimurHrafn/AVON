ba778e871bb14d9358114f5c48cbfecc7d8f0ac4
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-02 16:57:43 +0000

    Updated through script

--- rotary.py
+++ rotary.py
@@ -16,8 +16,8 @@ def rotary(left_or_right):
     print left_or_right
 
 GPIO.add_event_detect(35, GPIO.RISING, callback=test, bouncetime=100)
-GPIO.add_event_detect(left, GPIO.FALLING, callback=rotary)
-GPIO.add_event_detect(right, GPIO.FALLING, callback=rotary)
+GPIO.add_event_detect(left, GPIO.FALLING, callback=rotary, bouncetime=20)
+GPIO.add_event_detect(right, GPIO.FALLING, callback=rotary, bouncetime=20)
 
 
 while True:
