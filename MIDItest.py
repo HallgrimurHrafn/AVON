68bd60266a54bba0af0e4e5120c2cb886e86b7e1
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-09 17:16:21 +0000

    Updated through script

--- MIDItest.py
+++ MIDItest.py
@@ -7,10 +7,13 @@ import struct
 
 ser = serial.Serial(
     port='/dev/serial1', # thetta er serialportid a raspberry.
-    # baudrate = 38400,
-    # parity=serial.PARITY_NONE,       # latum RS3232 sja um thetta.
-    # stopbits=serial.STOPBITS_TWO,
-    # bytesize=serial.EIGHTBITS,
+    baudrate = 115200,
+    parity=serial.PARITY_NONE,       # latum RS3232 sja um thetta.
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    xonxoff=False,
+    rtscts=False,
+    dsrdtr=False
     # timeout=0
     )
 #counter =0
