1073800c6c1d6e7f0fcd21b83e9044859b6d4cfc
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-11 03:17:42 +0000

    Updated through script

--- Rotary.py
+++ Rotary.py
@@ -3,7 +3,7 @@ import time
 import threading
 import Main
 import menu
-
+import numpy as np
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -16,61 +16,66 @@ GPIO.setmode(GPIO.BOARD)
 # 0,1 : state 3
 # 0,0 : state 0  hrin er lokid her
 
-# rotary1
-cl1=0
-cr1=0
-state1=0
-fstate1=0  # former state1. sidasta astand semsagt.
-
-# rotary2
-cl2=0
-cr2=0
-state2=0
-fstate2=0  # former state2. sidasta astand semsagt.
+# fylki fyrir bada rotary
+cl=np.array([0, 0])
+cr=np.array([0, 0])
+state=np.array([0, 0])
+fstate=np.array([0, 0])   # former state. sidasta astand semsagt.
+l=np.array([33, "placeholder"])    # vantar gpio channel fyrir rotary 2.
+r=np.array([31, "placeholder"])
 
 # rotary 1
-GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary cl1ick
+GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary click
 GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary left
 GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary right
 
 # rotary 2
-# GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary cl1ick
+# GPIO.setup(35, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary click
 # GPIO.setup(33, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary left
 # GPIO.setup(31, GPIO.IN, pull_up_down=GPIO.PUD_UP) # rotary right
 
-def test(channel):
-    print('yeei')
 def rotary(channel):
-    global cl1, cr1, lock, fstate1, state1
-    if cl1 ==GPIO.input(33) and cr1==GPIO.input(31):  # erum vid i sama state-i?
-        return
-    cl1 = GPIO.input(33)     # ef ekki uppfaerum
-    cr1 = GPIO.input(31)
-    fstate1 = state1          # uppfaerum gamla astand.
-    if cl1==1 and cr1==0:     # uppfaerum astand og haettum ef astand er ekki 0.
-        state1=1
-        return
-    elif cl1==1 and cr1==1:
-        state1=2
-        return
-    elif cl1==0 and cr1==1:
-        state1=3
-        return
-    state1=0
-    if fstate1==1:
-        print 'right';
-    elif fstate1==3:
-        print 'left';
+    if channel==31 or channel==33 or channel==35:  # hvada rotary er ad senda.
+        i=0
     else:
-        print 'eitthvad for urskeidis.'
+        i=1
+    # placeholder verdur gpio fyrir rotary 2 click channel
+    if channel==31 or channel == "placeholder"
+        cd=True
+    else:
+        cd=False
+    if cd:
+        print "yeii", i        # click kom. af rotary <i>.
+    global cl, cr, lock, fstate, state
+    if cl[i] ==GPIO.input(33) and cr[i]==GPIO.input(31):  # erum vid i sama state-i?
+        return
+    cl[i] = GPIO.input(33)     # ef ekki uppfaerum
+    cr[i] = GPIO.input(31)
+    fstate[i] = state[i]          # uppfaerum gamla astand.
+    if cl[i]==1 and cr[i]==0:     # uppfaerum astand og haettum ef astand er ekki 0.
+        state[i]=1
+        return
+    elif cl[i]==1 and cr[i]==1:
+        state[i]=2
+        return
+    elif cl[i]==0 and cr[i]==1:
+        state[i]=3
+        return
+    state[i]=0
+    if fstate[i]==1:
+        print 'right', i
+    elif fstate[i]==3:
+        print 'left', i
+    else:
+        print 'eitthvad for urskeidis.', i
 
 # rotary 1
-GPIO.add_event_detect(35, GPIO.RISING, callback=test, bouncetime=100)
-GPIO.add_event_detect(33, GPIO.BOTH, callback=rotary1)
-GPIO.add_event_detect(31, GPIO.BOTH, callback=rotary1)
+GPIO.add_event_detect(35, GPIO.RISING, callback=rotary, bouncetime=100)
+GPIO.add_event_detect(33, GPIO.BOTH, callback=rotary)
+GPIO.add_event_detect(31, GPIO.BOTH, callback=rotary)
 
 # rotary 2
-# GPIO.add_event_detect(35, GPIO.RISING, callback=test, bouncetime=100)
+# GPIO.add_event_detect(35, GPIO.RISING, callback=rotary, bouncetime=100)
 # GPIO.add_event_detect(33, GPIO.BOTH, callback=rotary2)
 # GPIO.add_event_detect(31, GPIO.BOTH, callback=rotary2)
 
