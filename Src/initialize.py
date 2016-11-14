e4fc93e12041bf051b88c8f2c7ca5e2b05c4f62c
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-14 17:28:18 +0000

    Updated through rs script

--- Src/initialize.py
+++ Src/initialize.py
@@ -3,6 +3,18 @@ import menu
 import Rotary
 import Render
 import subprocess
+import threading
 
-subprocess.call(['python', 'Rotary.py'])
-Main.init()
+t1=threading.Thread(target=rot)
+t1.start()
+t2=threading.Thread(target=main)
+t2.start()
+
+def rot(channel):
+    subprocess.call(['python', 'Rotary.py'])
+
+def main(channel):
+    subprocess.call(['python', 'Main.py'])
+
+while True:
+    pass
