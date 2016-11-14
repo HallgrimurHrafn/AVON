c9947bbb1bbb6cec73f7cdd75361a27c1cf1c548
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-14 17:47:35 +0000

    Updated through rs script

--- Src/initialize.py
+++ Src/initialize.py
@@ -10,7 +10,8 @@ def main():
 
 t1=threading.Thread(target=rot)
 t1.start()
-t2=threading.Thread(target=main)
-t2.start()
+# t2=threading.Thread(target=main)
+# t2.start()
+
 while True:
     time.sleep(10)
