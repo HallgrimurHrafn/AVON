970719868046f0c73f13bb8a09415886d348b7d1
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-14 17:44:34 +0000

    Updated through rs script

--- Src/initialize.py
+++ Src/initialize.py
@@ -2,16 +2,15 @@ import subprocess
 import threading
 import time
 
-t1=threading.Thread(target=rot)
-t1.start()
-t2=threading.Thread(target=main)
-t2.start()
-
 def rot(channel):
     subprocess.call(['python', 'Rotary.py'])
 
 def main(channel):
     subprocess.call(['python', 'Main.py'])
 
+t1=threading.Thread(target=rot)
+t1.start()
+t2=threading.Thread(target=main)
+t2.start()
 while True:
     time.sleep(10)
