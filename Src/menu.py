d721ea6beb6a8784b59e6fa5c9781ca9abab45fb
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-15 20:25:44 +0000

    Updated through script

--- Src/menu.py
+++ Src/menu.py
@@ -93,10 +93,10 @@ def camera(val, xyz):
     Render.Render()
 
 def notelengdChange(val):
-    val=float(val/20)
+    val=float(val)/20
     if 0<Main.lengd+val<1:
         Main.lengd=Main.lengd+val
-        print Main.lengd 
+        print Main.lengd
         Render.Render()
 
 # def FLASHchange(val): # tharf ad adlaga fyrir prosentu
