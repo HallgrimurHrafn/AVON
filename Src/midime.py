1cada61d0aeb7ef2bc4cf085c118703abe5bad31
Avon Barksdale <halligrims@gmail.com>
2016-11-15 00:04:26 +0000

    Updated through rs script

--- Src/midime.py
+++ Src/midime.py
@@ -1,4 +1,6 @@
-def midime(cmd,note,val):
-	ser=serial.Serial('/dev/ttyAMA0',31250)
+import serial
+
+ser=serial.Serial('/dev/ttyAMA0',31250)
+def tm(cmd,note,val):
 	msg=chr(cmd)+chr(note)+chr(val)
 	ser.write(msg)
