6c7cf3b99da20cc699a15be9a0b261376617547e
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-16 23:22:25 +0000

    Updated through script

--- Src/menu.py
+++ Src/menu.py
@@ -142,7 +142,7 @@ def customsetup():  # glo.currentscale verdur ad vera staerra en 2.
         glo.navx=0
         if glo.es>0:
             glo.custom=np.array([
-            (glo.es-1)*8+glo.note,(glo.es-1)*8+1,
+            (glo.es-1)*8+glo.note,(glo.es-1)*8+1+glo.note,
             (glo.es-1)*8+2+glo.note,(glo.es-1)*8+3+glo.note,
             (glo.es-1)*8+4+glo.note,(glo.es-1)*8+5+glo.note,
             (glo.es-1)*8+6+glo.note,(glo.es-1)*8+7+glo.note
