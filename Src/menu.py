c8c7df7fa34ed413f52ffe92d438271639e4036a
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-17 00:07:40 +0000

    Updated through script

--- Src/menu.py
+++ Src/menu.py
@@ -170,8 +170,8 @@ def nyrskali():
     for i in range (0,7):
         if i%3==2:
             v+="\n"
-        v+="glo.note+"+str(glo.custom[i])+","
-    v+="glo.note+"+str(glo.custom[7]) +"]" + ")"
+        v+="glo.note +     "+str(glo.custom[i])+", "
+    v+="glo.note + "+str(glo.custom[7]) +"]" + ")"
     x="Main.newskali=np.array(["+v
     if glo.currentscale==3:
         glo.skalar=np.append(glo.skalar,x)
