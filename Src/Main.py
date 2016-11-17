48de5d35898a3b8ee0bf6e81d9d62775bb6ca59f
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-17 00:33:18 +0000

    Updated through script

--- Src/Main.py
+++ Src/Main.py
@@ -24,7 +24,8 @@ trellis.begin(
     (0x73, I2C_BUS)
     )
 
-# nonmenu taptemp = 1                     # hvort taptempo se virkt
+# nonmenu
+taptemp = 1                     # hvort taptempo se virkt
 v = 0                           # styring fyrir hvada voice vid aetlum a fara i.
 voice = 0                       # hvada voice er i notkun
 tkt = False                       # hvort ljosin fra taktmaelinum seu i gangi.
