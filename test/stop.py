4d6810c9facb83c3b71ed3b37c0cea8ced7cc460
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-20 17:14:46 +0000

    Updated through script

--- test/stop.py
+++ test/stop.py
@@ -2,13 +2,13 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set up STOP button
+GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up STOP button
 
 
 def stopper(channel):
     print "blah"
 
-GPIO.add_event_detect(38, GPIO.RISING, callback=stopper, bouncetime=100)
+GPIO.add_event_detect(38, GPIO.RISING, callback=stopper, bouncetime=200)
 
 while True:
     time.sleep(10)
