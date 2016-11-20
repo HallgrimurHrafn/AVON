24bc29f4464e31f5c8380203f3b467b05a35f234
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-20 16:57:59 +0000

    Updated through script

--- test/stop.py
+++ test/stop.py
@@ -5,7 +5,7 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up STOP button
 
 
-def stopper():
+def stopper(channel):
     print "blah"
 
 GPIO.add_event_detect(38, GPIO.FALLING, callback=stopper, bouncetime=200)
