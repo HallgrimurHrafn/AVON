3fef7357e5019de88c63a4ff0d23567df5a5235b
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-20 19:16:35 +0000

    Updated through script

--- ul.sh
+++ ul.sh
@@ -5,4 +5,4 @@ git add .
 git commit -m "Updated through script"
 git push origin master
 
-#install "script" package i atom og keyra med ctrl+shift+b
+#install "script" package i atom og keyra med ct  rl+shift+b
