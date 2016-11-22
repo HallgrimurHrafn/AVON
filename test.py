a19903e185ce3ab406f8f7313c63ce2a8bd740f8
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-22 00:41:13 +0000

    Updated through script

--- test.py
+++ test.py
@@ -19,9 +19,9 @@ GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up STOP button
 GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up START button
 GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up TAP button
 
-GPIO.add_event_detect(38, GPIO.BOTH, callback=stopper, bouncetime=200)
+GPIO.add_event_detect(38, GPIO.FALLING, callback=stopper, bouncetime=200)
 GPIO.add_event_detect(40, GPIO.FALLING, callback=playpause, bouncetime=200)
-GPIO.add_event_detect(36, GPIO.RISING, callback=callback_tap, bouncetime=200)
+GPIO.add_event_detect(36, GPIO.FALLING, callback=callback_tap, bouncetime=200)
 
 
 while True:
