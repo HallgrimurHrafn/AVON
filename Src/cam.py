e4f921985bb9ba7b8b9b133dc69d6b6cdfde93c2
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-22 03:44:00 +0000

    Updated through script

--- Src/cam.py
+++ Src/cam.py
@@ -4,6 +4,7 @@ import glo
 import threading
 import time
 import math
+import os
 
 
 
@@ -12,6 +13,27 @@ import math
 # function til ad breyta Main.x,y,z,seen.
 # prufa pitch function
 # control functions in general.
+
+def vision():
+    # os.getcwd()
+    # os.path.exists('/test2')
+    x=[False,3,2,1]
+    z=0
+
+
+    f = open(os.path.join('../vision/build', "XYZ.txt"), 'r')
+    for line in f:
+        if z==0:
+            if line == "True\n":
+                x[z]=True
+            else:
+                x[z]=True
+        else:
+            x[z]=int(line)
+        z+=1
+    if x[0]:
+        print x, x[1]+x[2]+x[3]
+
 
 def cam():
     while Main.cam:
