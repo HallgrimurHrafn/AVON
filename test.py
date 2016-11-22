745b5d6538a2e7e64176071c527218f520057095
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-22 00:40:26 +0000

    Updated through script

--- test.py
+++ test.py
@@ -15,13 +15,13 @@ def callback_tap(channel):
 
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set up STOP button
-GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set up START button
+GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up STOP button
+GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up START button
 GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up TAP button
 
 GPIO.add_event_detect(38, GPIO.BOTH, callback=stopper, bouncetime=200)
-GPIO.add_event_detect(40, GPIO.BOTH, callback=playpause, bouncetime=200)
-GPIO.add_event_detect(36, GPIO.BOTH, callback=callback_tap, bouncetime=200)
+GPIO.add_event_detect(40, GPIO.FALLING, callback=playpause, bouncetime=200)
+GPIO.add_event_detect(36, GPIO.RISING, callback=callback_tap, bouncetime=200)
 
 
 while True:
