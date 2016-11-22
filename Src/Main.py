7d7dd466570c5265be1e9b81e6c2923246ebbda2
Hallgrimur Hrafn Einarsson <halligrims@gmail.com>
2016-11-22 00:42:12 +0000

    Updated through script

--- Src/Main.py
+++ Src/Main.py
@@ -562,13 +562,13 @@ def init():
 
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up for trellis
-    GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set up STOP button
-    GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set up START button
-    GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # set up TAP button
+    GPIO.setup(38, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up STOP button
+    GPIO.setup(40, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up START button
+    GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP) # set up TAP button
 
-    GPIO.add_event_detect(38, GPIO.RISING, callback=stopper, bouncetime=200)
-    GPIO.add_event_detect(40, GPIO.RISING, callback=playpause, bouncetime=200)
-    GPIO.add_event_detect(36, GPIO.RISING, callback=callback_tap, bouncetime=200)
+    GPIO.add_event_detect(38, GPIO.FALLING, callback=stopper, bouncetime=200)
+    GPIO.add_event_detect(40, GPIO.FALLING, callback=playpause, bouncetime=200)
+    GPIO.add_event_detect(36, GPIO.FALLING, callback=callback_tap, bouncetime=200)
 
     for x in range(0, 64):
         trellis.clrLED(x)
