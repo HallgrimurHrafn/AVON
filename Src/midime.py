0105c26c25401e87aa60147aa4934e0cbc39cdc2
thatmooglie <bragimarinosson@gmail.com>
2017-03-29 22:26:15 +0000

    Updated through script

--- Src/midime.py
+++ Src/midime.py
@@ -2,10 +2,11 @@ import serial
 
 
 ser=serial.Serial('/dev/ttyAMA0',31250)
-def tm(cmd,note,val):
+def tm(144,64,127):
 	if note<0:
 		note=0
 	elif note>127:
 		note=127
 	msg=chr(cmd)+chr(note)+chr(val)
+	print(msg)
 	ser.write(msg)
