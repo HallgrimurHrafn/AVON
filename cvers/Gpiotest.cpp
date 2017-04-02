f7b03858c7f33392afd8111f021a98ef48652a4e
HallgrimurHrafn <halligrims@gmail.com>
2017-04-02 23:26:39 +0000

    Updated through rs script

--- cvers/Gpiotest.cpp
+++ cvers/Gpiotest.cpp
@@ -29,7 +29,7 @@ void supsuccess()
   auto tock = TIME::now();
   timer mismunur = tock-tick;
   ms milli = chrono::duration_cast<ms>(mismunur);
- if(milli> tvohundrudms)
+ if(milli> tvohundrudms/2)
  {
    thread test(success,0);
    test.detach();
