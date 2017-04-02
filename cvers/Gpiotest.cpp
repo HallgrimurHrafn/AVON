b4288b3031d231268fe52db8376379c777749531
HallgrimurHrafn <halligrims@gmail.com>
2017-04-02 21:43:28 +0000

    Updated through rs script

--- cvers/Gpiotest.cpp
+++ cvers/Gpiotest.cpp
@@ -24,8 +24,8 @@ int main() {
   pullUpDnControl(pin, PUD_UP);
   // wiringPiISR (pin, INT_EDGE_FALLING, &success(4));
   // wiringPiISR (pin, INT_EDGE_FALLING, &success2);
-  wiringPiISR (pin, INT_EDGE_FALLING, &success2());
-  // wiringPiISR (pin, INT_EDGE_FALLING, thread suc(success,100));
+  // wiringPiISR (pin, INT_EDGE_FALLING, &success2());
+  wiringPiISR (pin, INT_EDGE_FALLING, thread suc(success,100));
   for(;;)
   {
     usleep(1000000);
