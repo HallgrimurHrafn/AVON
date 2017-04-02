35e3e8d6d9d6019c83152bb8ed6fdd2b2c331a20
HallgrimurHrafn <halligrims@gmail.com>
2017-04-02 23:26:17 +0000

    Updated through rs script

--- cvers/Gpiotest.cpp
+++ cvers/Gpiotest.cpp
@@ -14,7 +14,7 @@
 using namespace std;
 
 typedef chrono::high_resolution_clock TIME;
-typedef chrono::milliseconds<int> ms;
+typedef chrono::milliseconds ms;
 typedef chrono::duration<float> timer;
 auto tick = TIME::now();
 ms tvohundrudms;
@@ -29,7 +29,7 @@ void supsuccess()
   auto tock = TIME::now();
   timer mismunur = tock-tick;
   ms milli = chrono::duration_cast<ms>(mismunur);
- if(milli> 200)
+ if(milli> tvohundrudms)
  {
    thread test(success,0);
    test.detach();
