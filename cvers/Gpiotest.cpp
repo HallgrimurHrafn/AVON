913d8d199cdb4582903013079434bdff0201a10c
HallgrimurHrafn <halligrims@gmail.com>
2017-04-02 21:40:37 +0000

    Updated through rs script

--- cvers/Gpiotest.cpp
+++ cvers/Gpiotest.cpp
@@ -12,9 +12,9 @@ int main() {
   int pin = 21;
   pinMode(pin, INPUT);
   pullUpDnControl(pin, PUD_UP);
-  wiringPiISR (pin, INT_EDGE_FALLING, &success(4));
+  // wiringPiISR (pin, INT_EDGE_FALLING, &success(4));
   // wiringPiISR (pin, INT_EDGE_FALLING, &success2);
-  // wiringPiISR (pin, INT_EDGE_FALLING, &success2());
+  wiringPiISR (pin, INT_EDGE_FALLING, &success2());
   // wiringPiISR (pin, INT_EDGE_FALLING, thread suc(success,100));
 
 
