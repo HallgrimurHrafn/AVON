8f7af8d648d7bc76f2a4493bde4afd2b96f6f8a7
HallgrimurHrafn <halligrims@gmail.com>
2017-04-02 22:07:48 +0000

    Updated through rs script

--- cvers/Gpiotest.cpp
+++ cvers/Gpiotest.cpp
@@ -7,15 +7,12 @@
 // #include <chrono>
 using namespace std;
 
-void success(int a)
+void success()
 {
-  cout << "OUTPUT! "<< a << endl;
+  cout << "OUTPUT! "<< endl;
 }
 
-void success2()
-{
-  thread suc(success,32);
-}
+
 int main() {
   wiringPiSetupGpio ();
 
@@ -23,7 +20,7 @@ int main() {
   pinMode(pin, INPUT);
   pullUpDnControl(pin, PUD_UP);
   // wiringPiISR (pin, INT_EDGE_FALLING, &success(4));
-  wiringPiISR (pin, INT_EDGE_FALLING, &success2);
+  wiringPiISR (pin, INT_EDGE_FALLING, &success);
   // wiringPiISR (pin, INT_EDGE_FALLING, &success2());
   // wiringPiISR (pin, INT_EDGE_FALLING, thread suc(success,100));
   for(;;)
