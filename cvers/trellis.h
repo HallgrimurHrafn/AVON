f6b4a54015ee22935168ee92be30141fa2873494
HallgrimurHrafn <halligrims@gmail.com>
2017-04-03 20:03:35 +0000

    Updated through rs script

--- cvers/trellis.h
+++ cvers/trellis.h
@@ -39,7 +39,7 @@ void writeDisplay(){
 
 bool pythonCatch(char const* command){
 	string stdOutErr =
-    " import sys\n\
+   " import sys\n\
      class CatchOut:\n\
         def __init__(self):\n\
            self.value = ''\n\
