b2ece76ad2227328c8fa4c1c03ec1b823e984e2f
thatmooglie <bragimarinosson@gmail.com>
2017-04-03 18:09:45 +0000

    Updated through rs script

--- cvers/trellis.h
+++ cvers/trellis.h
@@ -1,2 +1,35 @@
 #include <Python.h>
- 
+
+void initialize() {
+	Py_Initialize();
+	PyRun_SimpleString("import Adafruit_Trellis\n");
+	PyRun_SimpleString("\n");
+	PyRun_SimpleString("\n");
+	PyRun_SimpleString("\n");
+	PyRun_SimpleString("\n");
+	PyRun_SimpleString("\n");
+	PyRun_SimpleString("\n");
+	PyRun_SimpleString("\n");
+}
+
+void finalize() {
+	Py_Finalize();
+}
+
+void clrLed(int i){
+}
+
+void setLed(int i){
+}
+
+void writeDisplay(){
+}
+
+bool readSwitches(){
+}
+
+bool justPressed(){
+}
+
+bool justReleased(){
+}
