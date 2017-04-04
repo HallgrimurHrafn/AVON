860909e979cb86690afec5307b2593dd3f3832a5
HallgrimurHrafn <halligrims@gmail.com>
2017-04-04 18:24:50 +0000

    Updated through rs script

---
+++ cvers/cverision1/midi.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include "midime.h"
+
+using namespace std;
+
+int wiringPiSetup(void);
+
+int main()
+{
+	int note;
+	int val;
+	while(1<2)
+	{
+		cout << "Please select a note" << endl;
+		cin >> note;
+		midime(144,note,100);
+		usleep(100000);
+		midime(128,note,0);
+	}
+	return 0;
+}
