b4e427929a5c26a20b5a9673bdbc4002453e9742
HallgrimurHrafn <halligrims@gmail.com>
2017-04-05 00:02:07 +0000

    Updated through rs script

--- cvers/cverision1/changNav.h
+++ cvers/cverision1/changNav.h
@@ -8,17 +8,20 @@ extern int nav[];
 void changNav1()
 {
 	oldNav[nav[1]] = nav[0];
-	nav[] = {0,1};
+	nav[0] = 0;
+	nav[1] = 1;
 }
 void changNav2()
 {
 	oldNav[nav[1]] = nav[0];
-	nav[] = {0,2};
+	nav[0] = 0;
+	nav[1] = 2;
 }
 void changNav4()
 {
 	oldNav[nav[1]] = nav[0];
-	nav[] = {0,4};
+	nav[0] = 0;
+	nav[1] = 4;
 }
 
 #endif
